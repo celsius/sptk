2fda61fd2969787a5b3eae1af3061a054b55690e
Heiga Zen <heigazen@users.sourceforge.net>
2007-07-18 18:24:31 +0000

    Cleanup source codes.

--- src/bin/mcep/_mcep.c
+++ src/bin/mcep/_mcep.c
@@ -1,15 +1,15 @@
 /*
   ----------------------------------------------------------------
-	Speech Signal Processing Toolkit (SPTK): version 3.0
-			 SPTK Working Group
+   Speech Signal Processing Toolkit (SPTK): version 3.0
+          SPTK Working Group
 
-		   Department of Computer Science
-		   Nagoya Institute of Technology
-				and
+         Department of Computer Science
+         Nagoya Institute of Technology
+            and
     Interdisciplinary Graduate School of Science and Engineering
-		   Tokyo Institute of Technology
-		      Copyright (c) 1984-2000
-			All Rights Reserved.
+         Tokyo Institute of Technology
+            Copyright (c) 1984-2000
+         All Rights Reserved.
 
   Permission is hereby granted, free of charge, to use and
   distribute this software and its documentation without
@@ -43,20 +43,20 @@
 
     Mel-Cepstral Analysis
 
-	int mcep(xw, flng, mc, m, a, itr1, itr2, dd, e);
+    int mcep(xw, flng, mc, m, a, itr1, itr2, dd, e);
 
-	double   *xw  : input sequence
-	int      flng : frame length
-	double   *mc  : mel cepstrum
-	int      m    : order of mel cepstrum
-	double   a    : alpha
-	int      itr1 : minimum number of iteration
-	int      itr2 : maximum number of iteration
-	double   dd   : end condition
-	double   e    : initial value for log-periodgram
+    double   *xw  : input sequence
+    int      flng : frame length
+    double   *mc  : mel cepstrum
+    int      m    : order of mel cepstrum
+    double   a    : alpha
+    int      itr1 : minimum number of iteration
+    int      itr2 : maximum number of iteration
+    double   dd   : end condition
+    double   e    : initial value for log-periodgram
 
-	return value : 	0 -> completed by end condition
-			-1-> completed by maximum iteration
+    return value :    0 -> completed by end condition
+                      -1-> completed by maximum iteration
 
 *****************************************************************/
 
@@ -66,100 +66,100 @@
 
 int mcep(double *xw, int flng, double *mc, int m, double a, int itr1, int itr2, double dd, double e)
 {
-    register int   i, j;
-    int		   flag = 0, f2, m2;
-    double	   t, s, fabs(), exp(), log();
-    static double  *x = NULL, *y, *c, *d, *al, *b;
-    static int     size_x, size_d;
-    void	   frqtr();
+   int i, j;
+   int flag=0, f2, m2;
+   double t, s;
+   static double *x=NULL, *y, *c, *d, *al, *b;
+   static int size_x, size_d;
+   void frqtr(double *, int, double *, int, double);
   
-    if(x == NULL){
-	x = dgetmem(3*flng);
-	y = x + flng; c = y + flng;
-	size_x = flng;
-	
-	d = dgetmem(3*m+3);
-	al = d  + (m+1); b  = al + (m+1);
-	size_d = m;
-    }
-    if(flng > size_x){
-	free(x);
-	x = dgetmem(3*flng);
-	y = x + flng; c = y + flng;
-	size_x = flng;
-    }
-    if(m > size_d){
-	free(d);
-	d = dgetmem(3*m+3);
-	al = d  + (m+1); b  = al + (m+1);
-	size_d = m;
-    }
+   if (x==NULL) {
+      x = dgetmem(3*flng);
+      y = x + flng; c = y + flng;
+      size_x = flng;
+   
+      d = dgetmem(3*m+3);
+      al = d  + (m+1); b  = al + (m+1);
+      size_d = m;
+   }
+   if (flng > size_x) {
+      free(x);
+      x = dgetmem(3*flng);
+      y = x + flng; c = y + flng;
+      size_x = flng;
+   }
+   if (m > size_d) {
+      free(d);
+      d = dgetmem(3*m+3);
+      al = d  + (m+1); b  = al + (m+1);
+      size_d = m;
+   }
 
-    f2 = flng / 2;
-    m2 = m + m;
+   f2 = flng / 2;
+   m2 = m + m;
 
-    movem(xw, x, sizeof(*x), flng);
+   movem(xw, x, sizeof(*x), flng);
 
-    /*  power spectrum  */
-    fftr(x, y, flng);
-    for(i=0; i<flng; i++){
-	x[i] = x[i]*x[i] + y[i]*y[i] + e;
-	if(x[i] <= 0){
-	   fprintf(stderr,"mcep : The log periodogram has the value of '0' consider to use '-e' option !\n");
-	   exit(0);
-	}
-	c[i] = log(x[i]);
-    }
-	
-    /*  1, (-a), (-a)^2, ..., (-a)^M  */
-    al[0] = 1.0;
-    for(i=1; i<=m; i++)
-	al[i] = -a * al[i-1];
+   /*  power spectrum  */
+   fftr(x, y, flng);
+   for (i=0; i<flng; i++) {
+      x[i] = x[i]*x[i] + y[i]*y[i] + e;
+      if (x[i] <= 0) {
+         fprintf(stderr,"mcep : The log periodogram has the value of '0' consider to use '-e' option !\n");
+         exit(0);
+      }
+      c[i] = log(x[i]);
+   }
+   
+   /*  1, (-a), (-a)^2, ..., (-a)^M  */
+   al[0] = 1.0;
+   for (i=1; i<=m; i++)
+      al[i] = -a * al[i-1];
 
-    /*  initial value of cepstrum  */
-    ifftr(c, y, flng);				/*  c : IFFT[x]  */
-    
-    c[0] /= 2.0;
-    c[flng/2] /= 2.0;
-    freqt(c, f2, mc, m, a);			/*  mc : mel cep.  */
-    s = c[0];
+   /*  initial value of cepstrum  */
+   ifftr(c, y, flng);            /*  c : IFFT[x]  */
+   
+   c[0] /= 2.0;
+   c[flng/2] /= 2.0;
+   freqt(c, f2, mc, m, a);         /*  mc : mel cep.  */
+   s = c[0];
 
-/*  Newton Raphson method  */
-    for (j=1; j<=itr2; j++){
-	fillz(c, sizeof(*c), flng);
-	freqt(mc, m, c, f2, -a);		/*  mc : mel cep.  */
-	fftr(c, y, flng);			/*  c, y : FFT[mc]  */
-	for(i=0; i<flng; i++)
-	    c[i] = x[i] / exp(c[i] + c[i]);
-	ifftr(c, y, flng);
-	frqtr(c, f2, c, m2, a);			/*  c : r(k)  */
+   /*  Newton Raphson method  */
+   for (j=1; j<=itr2; j++) {
+      fillz(c, sizeof(*c), flng);
+      freqt(mc, m, c, f2, -a);      /*  mc : mel cep.  */
+      fftr(c, y, flng);         /*  c, y : FFT[mc]  */
+      for (i=0; i<flng; i++)
+         c[i] = x[i] / exp(c[i] + c[i]);
+      ifftr(c, y, flng);
+      frqtr(c, f2, c, m2, a);         /*  c : r(k)  */
 
-	t = c[0];
-	if(j >= itr1){
-	    if (fabs((t - s)/t) < dd){
-		 flag = 1;
-		 break;
-	    }
-	    s = t;
-	}
-	
-	for(i=0; i<=m; i++)
-	    b[i] = c[i] - al[i];
-	for(i=0; i<=m2; i++)  y[i] = c[i];
-	for(i=0; i<=m2; i+=2) y[i] -= c[0];
-	for(i=2; i<=m;  i+=2) c[i] += c[0];
-	c[0] += c[0];
+      t = c[0];
+      if (j >= itr1) {
+         if (fabs((t - s)/t) < dd) {
+            flag = 1;
+            break;
+         }
+         s = t;
+      }
+   
+      for (i=0; i<=m; i++)
+         b[i] = c[i] - al[i];
+      for (i=0; i<=m2; i++)  y[i] = c[i];
+      for (i=0; i<=m2; i+=2) y[i] -= c[0];
+      for (i=2; i<=m;  i+=2) c[i] += c[0];
+      c[0] += c[0];
 
-	if (theq(c, y, d, b, m+1, -1.0)){
-	    fprintf(stderr,"mcep : Error in theq() at %dth iteration !\n", j);
-	    exit(1);
-	}
+      if (theq(c, y, d, b, m+1, -1.0)) {
+         fprintf(stderr,"mcep : Error in theq() at %dth iteration !\n", j);
+         exit(1);
+      }
 
-	for (i=0; i<=m; i++) mc[i] += d[i];
-    }
+      for (i=0; i<=m; i++) mc[i] += d[i];
+   }
 
-    if(flag) return(0);
-    else     return(-1);
+   if (flag) return(0);
+   else     return(-1);
 
 }
 
@@ -167,45 +167,45 @@ int mcep(double *xw, int flng, double *mc, int m, double a, int itr1, int itr2, 
 
     Frequency Transformation for Calculating Coefficients
 
-	void	frqtr(c1, m1, c2, m2, a)
+    void frqtr(c1, m1, c2, m2, a)
 
-	double	*c1   : minimum phase sequence
-	int	m1    : order of minimum phase sequence
-	double	*c2   : warped sequence
-	int	m2    : order of warped sequence
-	double	a     : all-pass constant
+    double *c1   : minimum phase sequence
+    int m1       : order of minimum phase sequence
+    double *c2   : warped sequence
+    int m2       : order of warped sequence
+    double a     : all-pass constant
 
 ***************************************************************/
 
-void frqtr(double *c1, int m1, double *c2, int m2, double a)
+void frqtr (double *c1, int m1, double *c2, int m2, double a)
 {
-    register int 	i, j;
-    static double	*d = NULL, *g;
-    static int		size;
-    
-    if(d == NULL){
-	size = m2;
-	d = dgetmem(size+size+2);
-	g = d + size + 1;
-    }
+   int i, j;
+   static double *d=NULL, *g;
+   static int size;
+   
+   if (d==NULL) {
+      size = m2;
+      d = dgetmem(size+size+2);
+      g = d + size + 1;
+   }
 
-    if(m2 > size){
-	free(d);
-	size = m2;
-	d = dgetmem(size+size+2);
-	g = d + size + 1;
-    }
+   if (m2>size) {
+      free(d);
+      size = m2;
+      d = dgetmem(size+size+2);
+      g = d + size + 1;
+   }
 
-    fillz(g, sizeof(*g), m2+1);
+   fillz(g, sizeof(*g), m2+1);
 
-    for (i=-m1; i<=0; i++){
-	if (0 <= m2){
-	    d[0] = g[0];
-	    g[0] = c1[-i];
-	}
-	for (j=1; j<=m2; j++)
-	    g[j] = d[j-1] + a*((d[j] = g[j]) - g[j-1]);
-    }
+   for (i=-m1; i<=0; i++) {
+      if (0 <= m2) {
+         d[0] = g[0];
+         g[0] = c1[-i];
+      }
+      for (j=1; j<=m2; j++)
+         g[j] = d[j-1] + a*((d[j] = g[j]) - g[j-1]);
+   }
 
-    movem(g, c2, sizeof(*g), m2+1);
+   movem(g, c2, sizeof(*g), m2+1);
 }
--- src/bin/mcep/mcep.c
+++ src/bin/mcep/mcep.c
@@ -1,15 +1,15 @@
 /*
   ----------------------------------------------------------------
-	Speech Signal Processing Toolkit (SPTK): version 3.0
-			 SPTK Working Group
+   Speech Signal Processing Toolkit (SPTK): version 3.0
+         SPTK Working Group
 
-		   Department of Computer Science
-		   Nagoya Institute of Technology
-				and
-    Interdisciplinary Graduate School of Science and Engineering
-		   Tokyo Institute of Technology
-		      Copyright (c) 1984-2000
-			All Rights Reserved.
+        Department of Computer Science
+        Nagoya Institute of Technology
+           and
+   Interdisciplinary Graduate School of Science and Engineering
+        Tokyo Institute of Technology
+           Copyright (c) 1984-2000
+        All Rights Reserved.
 
   Permission is hereby granted, free of charge, to use and
   distribute this software and its documentation without
@@ -18,10 +18,10 @@
   sell copies of this work, and to permit persons to whom this
   work is furnished to do so, subject to the following conditions:
 
-    1. The code must retain the above copyright notice, this list
-       of conditions and the following disclaimer.
+   1. The code must retain the above copyright notice, this list
+      of conditions and the following disclaimer.
 
-    2. Any modifications must be clearly marked as such.
+   2. Any modifications must be clearly marked as such.
 
   NAGOYA INSTITUTE OF TECHNOLOGY, TOKYO INSITITUTE OF TECHNOLOGY,
   SPTK WORKING GROUP, AND THE CONTRIBUTORS TO THIS WORK DISCLAIM
@@ -38,32 +38,32 @@
 */
 
 /************************************************************************
-*									*
-*    Mel Cepstral Analysis						*
-*									*
-*					1993.5  K.Tokuda		*
-*					1996.1  K.Koishida		*
-*									*
-*	usage:								*
-*		mcep [ options ] [infile] > stdout			*
-*	options:							*
-*		-a alpha :  all-pass constant		     [0.35]	*
-*		-m m     :  order of mel cepstrum	     [25]	*
-*		-l l     :  frame length		     [256]	*
-*		(level 2)						*
-*		-i i     :  minimum iteration		     [2]	*
-*		-j j     :  maximum iteration		     [30]	*
-*		-d d     :  end condition		     [0.001]	*
-*		-e e     :  small value added to periodgram [0.0]	*
-*	infile:								*
-*		data sequence						*
-*		    , x(0), x(1), ..., x(L-1),				*
-*	stdout:								*
-*		mel cepstrum						*
-*		    , c~(0), c~(1), ..., c~(M),				*
-*	require:							*
-*		mcep()							*
-*									*
+*                                                                       *
+*    Mel Cepstral Analysis                                              *
+*                                                                       *
+*               1993.5  K.Tokuda                                        *
+*               1996.1  K.Koishida                                      *
+*                                                                       *
+*   usage:                                                              *
+*      mcep [ options ] [infile] > stdout                               *
+*   options:                                                            *
+*      -a alpha :  all-pass constant               [0.35]               *
+*      -m m     :  order of mel cepstrum           [25]                 *
+*      -l l     :  frame length                    [256]                *
+*      (level 2)                                                        *
+*      -i i     :  minimum iteration               [2]                  *
+*      -j j     :  maximum iteration               [30]                 *
+*      -d d     :  end condition                   [0.001]              *
+*      -e e     :  small value added to periodgram [0.0]                *
+*   infile:                                                             *
+*      data sequence                                                    *
+*          , x(0), x(1), ..., x(L-1),                                   *
+*   stdout:                                                             *
+*      mel cepstrum                                                     *
+*          , c~(0), c~(1), ..., c~(M),                                  *
+*   require:                                                            *
+*      mcep()                                                           *
+*                                                                       *
 ************************************************************************/
 
 static char *rcs_id = "$Id$";
@@ -76,111 +76,111 @@ static char *rcs_id = "$Id$";
 #include <SPTK.h>
 
 /*  Default Values  */
-#define	ALPHA		0.35
-#define ORDER		25
-#define FLENG           256
-#define MINITR          2
-#define MAXITR          30
-#define END             0.001
-#define EPS             0.0
+#define ALPHA 0.35
+#define ORDER 25
+#define FLENG 256
+#define MINITR 2
+#define MAXITR 30
+#define END 0.001
+#define EPS 0.0
 
 
 /*  Command Name  */
-char	*cmnd;
+char *cmnd;
 
 
 void usage(int status)
 {
-    fprintf(stderr, "\n");
-    fprintf(stderr, " %s - mel cepstral analysis\n",cmnd);
-    fprintf(stderr, "\n");
-    fprintf(stderr, "  usage:\n");
-    fprintf(stderr, "       %s [ options ] [ infile ] > stdout\n", cmnd);
-    fprintf(stderr, "  options:\n");
-    fprintf(stderr, "       -a a  : all-pass constant               [%g]\n", ALPHA);
-    fprintf(stderr, "       -m m  : order of mel cepstrum           [%d]\n", ORDER);
-    fprintf(stderr, "       -l l  : frame length                    [%d]\n", FLENG);
-    fprintf(stderr, "       -h    : print this message\n");
-    fprintf(stderr, "     (level 2)\n");
-    fprintf(stderr, "       -i i  : minimum iteration               [%d]\n", MINITR);
-    fprintf(stderr, "       -j j  : maximum iteration               [%d]\n", MAXITR);
-    fprintf(stderr, "       -d d  : end condition                   [%g]\n", END);
-    fprintf(stderr, "       -e e  : small value added to periodgram [%g]\n",EPS);
-    fprintf(stderr, "  infile:\n");
-    fprintf(stderr, "       windowed sequences (float)              [stdin]\n");
-    fprintf(stderr, "  stdout:\n");
-    fprintf(stderr, "       mel-cepstrum (float)\n");
+   fprintf(stderr, "\n");
+   fprintf(stderr, " %s - mel cepstral analysis\n",cmnd);
+   fprintf(stderr, "\n");
+   fprintf(stderr, "  usage:\n");
+   fprintf(stderr, "       %s [ options ] [ infile ] > stdout\n", cmnd);
+   fprintf(stderr, "  options:\n");
+   fprintf(stderr, "       -a a  : all-pass constant               [%g]\n", ALPHA);
+   fprintf(stderr, "       -m m  : order of mel cepstrum           [%d]\n", ORDER);
+   fprintf(stderr, "       -l l  : frame length                    [%d]\n", FLENG);
+   fprintf(stderr, "       -h    : print this message\n");
+   fprintf(stderr, "     (level 2)\n");
+   fprintf(stderr, "       -i i  : minimum iteration               [%d]\n", MINITR);
+   fprintf(stderr, "       -j j  : maximum iteration               [%d]\n", MAXITR);
+   fprintf(stderr, "       -d d  : end condition                   [%g]\n", END);
+   fprintf(stderr, "       -e e  : small value added to periodgram [%g]\n",EPS);
+   fprintf(stderr, "  infile:\n");
+   fprintf(stderr, "       windowed sequences (float)              [stdin]\n");
+   fprintf(stderr, "  stdout:\n");
+   fprintf(stderr, "       mel-cepstrum (float)\n");
 #ifdef SPTK_VERSION
-    fprintf(stderr, "\n");
-    fprintf(stderr, " SPTK: version %s",SPTK_VERSION);
+   fprintf(stderr, "\n");
+   fprintf(stderr, " SPTK: version %s",SPTK_VERSION);
 #endif
-    fprintf(stderr, "\n");
-    exit(status);
+   fprintf(stderr, "\n");
+   exit(status);
 }
 
 
 int main(int argc, char **argv)
 {
-    int	     m = ORDER, flng = FLENG, itr1 = MINITR, itr2 = MAXITR, flag = 0;
-    FILE     *fp = stdin;
-    double   *mc, *x, a = ALPHA, end = END, e = EPS, atof();    
+   int m=ORDER, flng=FLENG, itr1=MINITR, itr2=MAXITR, flag=0;
+   FILE *fp = stdin;
+   double *mc, *x, a=ALPHA, end=END, e=EPS;    
 
-    if ((cmnd = strrchr(argv[0], '/')) == NULL)
-	cmnd = argv[0];
-    else
-	cmnd++;
-    while (--argc)
-	if (**++argv == '-') {
-	    switch (*(*argv+1)) {
-	        case 'a':
-		    a = atof(*++argv);
-		    --argc;
-		    break;
-		case 'm':
-		    m = atoi(*++argv);
-		    --argc;
-		    break;
-		case 'l':
-		    flng = atoi(*++argv);
-		    --argc;
-		    break;
-		case 'i':
-		    itr1 = atoi(*++argv);
-		    --argc;
-		    break;
-		case 'j':
-		    itr2 = atoi(*++argv);
-		    --argc;
-		    break;
-		case 'd':
-		    end = atof(*++argv);
-		    --argc;
-		    break;
-		case 'e':
-		    e = atof(*++argv);
-		    --argc;
-		    break;
-		case 'h':
-		    usage(0);
-		default:
-		    fprintf(stderr, "%s : Invalid option '%c' !\n", cmnd, *(*argv+1));
-		    usage(1);
-		}
-	}
-	else 
-	    fp = getfp(*argv, "r");
+   if ((cmnd = strrchr(argv[0], '/')) == NULL)
+      cmnd = argv[0];
+   else
+      cmnd++;
+   while (--argc)
+      if (**++argv == '-') {
+         switch (*(*argv+1)) {
+         case 'a':
+            a = atof(*++argv);
+            --argc;
+            break;
+         case 'm':
+            m = atoi(*++argv);
+            --argc;
+            break;
+         case 'l':
+            flng = atoi(*++argv);
+            --argc;
+            break;
+         case 'i':
+            itr1 = atoi(*++argv);
+            --argc;
+            break;
+         case 'j':
+            itr2 = atoi(*++argv);
+            --argc;
+            break;
+         case 'd':
+            end = atof(*++argv);
+            --argc;
+            break;
+         case 'e':
+            e = atof(*++argv);
+            --argc;
+            break;
+         case 'h':
+            usage(0);
+         default:
+            fprintf(stderr, "%s : Invalid option '%c' !\n", cmnd, *(*argv+1));
+            usage(1);
+         }
+      }
+      else 
+         fp = getfp(*argv, "r");
 
-    x = dgetmem(flng+m+1);
-    mc = x + flng;
-    
-    while (freadf(x, sizeof(*x), flng, fp) == flng){
-	flag = mcep(x, flng, mc, m, a, itr1, itr2, end, e);
-	fwritef(mc, sizeof(*mc), m+1, stdout);
-    }
-    if(flag == 0)
-    	fprintf(stderr, "completed by end condition\n");
-    else if(flag == -1)
-	fprintf(stderr, "completed by maximum iteration\n");			
-    exit(0);
+   x = dgetmem(flng+m+1);
+   mc = x + flng;
+   
+   while (freadf(x, sizeof(*x), flng, fp)==flng) {
+      flag = mcep(x, flng, mc, m, a, itr1, itr2, end, e);
+      fwritef(mc, sizeof(*mc), m+1, stdout);
+   }
+   if(flag==0)
+      fprintf(stderr, "completed by end condition\n");
+   else if(flag==-1)
+      fprintf(stderr, "completed by maximum iteration\n");         
+   exit(0);
 }
 
