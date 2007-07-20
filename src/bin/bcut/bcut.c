92beb7e1fbeff60b87f0bac85a73fed3803afc21
heigazen <heigazen>
2007-07-20 08:38:05 +0000

    Cleanup source codes.

--- src/bin/bcut/bcut.c
+++ src/bin/bcut/bcut.c
@@ -1,15 +1,15 @@
 /*
   ----------------------------------------------------------------
-	Speech Signal Processing Toolkit (SPTK): version 3.0
-			 SPTK Working Group
+Speech Signal Processing Toolkit (SPTK): version 3.0
+SPTK Working Group
 
-		   Department of Computer Science
-		   Nagoya Institute of Technology
-				and
+Department of Computer Science
+Nagoya Institute of Technology
+and
     Interdisciplinary Graduate School of Science and Engineering
-		   Tokyo Institute of Technology
-		      Copyright (c) 1984-2000
-			All Rights Reserved.
+Tokyo Institute of Technology
+Copyright (c) 1984-2000
+All Rights Reserved.
 
   Permission is hereby granted, free of charge, to use and
   distribute this software and its documentation without
@@ -65,7 +65,7 @@
 *									*
 ************************************************************************/
 
-static char *rcs_id = "$Id: bcut.c,v 1.6 2006/12/21 07:23:13 mr_alex Exp $";
+static char *rcs_id = "$Id: bcut.c,v 1.7 2007/07/20 08:38:05 heigazen Exp $";
 
 
 /*  Standard C Libraries  */
@@ -77,126 +77,126 @@ static char *rcs_id = "$Id: bcut.c,v 1.6 2006/12/21 07:23:13 mr_alex Exp $";
 
 
 /*  Default Values  */
-#define START		0
-#define END		-1
-#define LENG		1
+#define START 0
+#define END -1
+#define LENG 1
 
 
 /*  Command Name  */
-char	*cmnd;
+char *cmnd;
 
 
-void usage(int status)
+void usage (int status)
 {
-    fprintf(stderr, "\n");
-    fprintf(stderr, " %s - binary file cut\n",cmnd);
-    fprintf(stderr, "\n");
-    fprintf(stderr, "  usage:\n");
-    fprintf(stderr, "       %s [ options ] [ infile ] > stdout\n", cmnd);
-    fprintf(stderr, "  options:\n");
-    fprintf(stderr, "       -s s  : start number [%d]\n",START);
-    fprintf(stderr, "       -e e  : end number   [EOF]\n");
-    fprintf(stderr, "       -l l  : block length [%d]\n",LENG);
-    fprintf(stderr, "       -n n  : block order  [l-1]\n");
-    fprintf(stderr, "       +type : data type    [f]\n");
-    fprintf(stderr, "                c (char)      s (short)\n");
-    fprintf(stderr, "                i (int)       l (long)\n");
-    fprintf(stderr, "                f (float)     d (double)\n");
-    fprintf(stderr, "       -h    : print this message\n");
-    fprintf(stderr, "  infile:\n");
-    fprintf(stderr, "       data sequence        [stdin]\n");
-    fprintf(stderr, "  stdout:\n");
-    fprintf(stderr, "       cut data sequence\n");
-    fprintf(stderr, "  note:\n");
-    fprintf(stderr, "       When both -l and -n are specified,\n");
-    fprintf(stderr, "       latter argument is adopted.\n");
+   fprintf(stderr, "\n");
+   fprintf(stderr, " %s - binary file cut\n",cmnd);
+   fprintf(stderr, "\n");
+   fprintf(stderr, "  usage:\n");
+   fprintf(stderr, "       %s [ options ] [ infile ] > stdout\n", cmnd);
+   fprintf(stderr, "  options:\n");
+   fprintf(stderr, "       -s s  : start number [%d]\n",START);
+   fprintf(stderr, "       -e e  : end number   [EOF]\n");
+   fprintf(stderr, "       -l l  : block length [%d]\n",LENG);
+   fprintf(stderr, "       -n n  : block order  [l-1]\n");
+   fprintf(stderr, "       +type : data type    [f]\n");
+   fprintf(stderr, "                c (char)      s (short)\n");
+   fprintf(stderr, "                i (int)       l (long)\n");
+   fprintf(stderr, "                f (float)     d (double)\n");
+   fprintf(stderr, "       -h    : print this message\n");
+   fprintf(stderr, "  infile:\n");
+   fprintf(stderr, "       data sequence        [stdin]\n");
+   fprintf(stderr, "  stdout:\n");
+   fprintf(stderr, "       cut data sequence\n");
+   fprintf(stderr, "  note:\n");
+   fprintf(stderr, "       When both -l and -n are specified,\n");
+   fprintf(stderr, "       latter argument is adopted.\n");
 #ifdef SPTK_VERSION
-    fprintf(stderr, "\n");
-    fprintf(stderr, " SPTK: version %s",SPTK_VERSION);
+   fprintf(stderr, "\n");
+   fprintf(stderr, " SPTK: version %s\n", SPTK_VERSION);
+   fprintf(stderr, "  %s", rcs_id);
 #endif
-    fprintf(stderr, "\n");
-    exit(status);
+   fprintf(stderr, "\n");
+   exit(status);
 }
 
-int main(int argc, char **argv)
+int main (int argc, char **argv)
 {
-    int		   size = sizeof(float), n = LENG;
-    long 	   start = START, end = END, ptr, atol();
-    FILE 	   *fp = stdin;
-    register char  *s, c;
-    double     	   x;
+   int size=sizeof(float), n=LENG;
+   long start=START, end=END, ptr;
+   FILE *fp=stdin;
+   char *s, c;
+   double x;
+        
+   if ((cmnd = strrchr(argv[0], '/'))==NULL)
+      cmnd = argv[0];
+   else
+      cmnd++;
+   while (--argc)
+      if (*(s = *++argv)=='-') {
+         c = *++s;
+         switch(c) {
+         case 's':
+            start = atol(*++argv);
+            --argc;
+            break;
+         case 'e':
+            end = atol(*++argv);
+            --argc;
+            break;
+         case 'l':
+            n = atol(*++argv);
+            --argc;
+            break;
+         case 'n':
+            n = atol(*++argv)+1;
+            --argc;
+            break;
+         case 'h':
+            usage(0);
+         default:
+            fprintf(stderr, "%s : Invalid option '%c' !\n", cmnd, *(*argv+1));
+            usage(1);
+         }
+      }
+      else if (*s=='+') {
+         c = *++s;
+         switch(c) {
+         case 'c':
+            size = sizeof(char);
+            break;
+         case 's':
+            size = sizeof(short);
+            break;
+         case 'l':
+            size = sizeof(long);
+            break;
+         case 'i':
+            size = sizeof(int);
+            break;
+         case 'f':
+            size = sizeof(float);
+            break;
+         case 'd':
+            size = sizeof(double);
+            break;
+         default:
+            fprintf(stderr, "%s : Invalid option '%c' !\n", cmnd, *(*argv+1));
+            usage(1);
+         }
+      }
+      else
+         fp = getfp(*argv, "r");
+      
+   ptr = start * n;
+   while(ptr--)
+      if (fread(&x, size, 1, fp) != 1) exit(0);
     
-    
-     if ((cmnd = strrchr(argv[0], '/')) == NULL)
-	cmnd = argv[0];
-    else
-	cmnd++;
-    while (--argc)
-	if(*(s = *++argv) == '-') {
-	    c = *++s;
-	    switch(c) {
-		case 's':
-		    start = atol(*++argv);
-		    --argc;
-		    break;
-		case 'e':
-		    end = atol(*++argv);
-		    --argc;
-		    break;
-		case 'l':
-		    n = atol(*++argv);
-		    --argc;
-		    break;
-		case 'n':
-		    n = atol(*++argv)+1;
-		    --argc;
-		    break;
-		case 'h':
-		    usage(0);
-		default:
-		    fprintf(stderr, "%s : Invalid option '%c' !\n", cmnd, *(*argv+1));
-		    usage(1);
-		}
-	}
-	else if (*s == '+') {
-	    c = *++s;
-	    switch(c) {
-		case 'c':
-		    size = sizeof(char);
-		    break;
-		case 's':
-		    size = sizeof(short);
-		    break;
-		case 'l':
-		    size = sizeof(long);
-		    break;
-		case 'i':
-		    size = sizeof(int);
-		    break;
-		case 'f':
-		    size = sizeof(float);
-		    break;
-		case 'd':
-		    size = sizeof(double);
-		    break;
-		default:
-		    fprintf(stderr, "%s : Invalid option '%c' !\n", cmnd, *(*argv+1));
-		    usage(1);
-	    }
-	}
-	else
-	    fp = getfp(*argv, "r");
+   ptr = (end - start + 1) * n;
+   while (end==-1 || ptr--) {
+      if (fread(&x, size, 1, fp)!=1)
+         break;
+      fwrite(&x, size, 1, stdout);
+   }
 
-    ptr = start * n;
-    while(ptr--)
-	if(fread(&x, size, 1, fp) != 1) exit(0);
-    
-    ptr = (end - start + 1) * n;
-    while (end == -1 || ptr--){
-	if (fread(&x, size, 1, fp) != 1)
-	    break;
-	fwrite(&x, size, 1, stdout);
-    }
-
-    exit(0);
+   exit(0);
 }
