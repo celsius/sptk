3179a3bbc395aa21388299b69f4e391321b66593
Heiga Zen <heigazen@users.sourceforge.net>
2007-07-23 04:30:27 +0000

    Cleanup source codes.

--- src/bin/clip/clip.c
+++ src/bin/clip/clip.c
@@ -58,6 +58,8 @@
 *									*
 ************************************************************************/
 
+static char *rcs_id = "$Id$";
+
 
 /* Standard C Libraries */
 #include <stdio.h>
@@ -69,85 +71,87 @@
 /* Default Value */
 #define FLT_MAX 3.4e+38
 #define FLT_MIN -3.4e+38
-#define YMIN	-1.0
-#define YMAX	1.0
+#define YMIN -1.0
+#define YMAX 1.0
 
 
 /* Command Name */
-char	*cmnd;
+char *cmnd;
 
-void usage()
+void usage (int status)
 {
-	fprintf(stderr, "\n");
-	fprintf(stderr, " %s - data clipping\n", cmnd);
-	fprintf(stderr, "\n");
-	fprintf(stderr, "  usage:\n");
-	fprintf(stderr, "       %s [ option ] [ infile ] > outfile\n", cmnd);
-	fprintf(stderr, "  option:\n");
-	fprintf(stderr, "       -y    ymin ymax : lower bound & upper bound [-1.0 1.0]\n");
-	fprintf(stderr, "       -ymin ymin      : lower bound (ymax = inf)  [N/A]\n");
-	fprintf(stderr, "       -ymax ymax      : upper bound (ymin = -inf) [N/A]\n");
-	fprintf(stderr, "       -h              : print this message\n");
-	fprintf(stderr, "  infile:\n");
-	fprintf(stderr, "       data sequence (float)                       [stdin]\n");
-	fprintf(stderr, "  stdout:\n");
-	fprintf(stderr, "       clipped data sequence (float)\n");
+   fprintf(stderr, "\n");
+   fprintf(stderr, " %s - data clipping\n", cmnd);
+   fprintf(stderr, "\n");
+   fprintf(stderr, "  usage:\n");
+   fprintf(stderr, "       %s [ option ] [ infile ] > outfile\n", cmnd);
+   fprintf(stderr, "  option:\n");
+   fprintf(stderr, "       -y    ymin ymax : lower bound & upper bound [-1.0 1.0]\n");
+   fprintf(stderr, "       -ymin ymin      : lower bound (ymax = inf)  [N/A]\n");
+   fprintf(stderr, "       -ymax ymax      : upper bound (ymin = -inf) [N/A]\n");
+   fprintf(stderr, "       -h              : print this message\n");
+   fprintf(stderr, "  infile:\n");
+   fprintf(stderr, "       data sequence (float)                       [stdin]\n");
+   fprintf(stderr, "  stdout:\n");
+   fprintf(stderr, "       clipped data sequence (float)\n");
 #ifdef SPTK_VERSION
-	fprintf(stderr, "\n");
-	fprintf(stderr, " SPTK: version %s",SPTK_VERSION);
+   fprintf(stderr, "\n");
+   fprintf(stderr, " SPTK: version %s\n", SPTK_VERSION);
+   fprintf(stderr, " CVS Info: %s", rcs_id);
 #endif
-	fprintf(stderr, "\n");
-	exit(1);
+   fprintf(stderr, "\n");
+   exit(status);
 }
 
-int main(int argc, char *argv[])
+int main (int argc, char *argv[])
 {
-        FILE    *fp = stdin;
-	int	size;
-	double	ymin = YMIN, ymax = YMAX;
-	double	*x, *y;
-	double	atof();
+   FILE *fp=stdin;
+   int size;
+   double ymin=YMIN, ymax=YMAX;
+   double *x, *y;
 
-	if ((cmnd = strrchr(argv[0],'/')) == NULL)
-	    cmnd = argv[0];
-	else
-	    cmnd++;
-	while (--argc)
-		if (**++argv == '-'){
-			argc--;
-			switch (*(*argv+1)) {
-			case 'y':
-				if((*(*argv+2))=='m'){
-					switch (*(*argv+3)) {
-						case 'i':
-							ymin = atof(*++argv);
-							ymax = FLT_MAX;
-							break;
-						case 'a':
-							ymax = atof(*++argv);
-							ymin = FLT_MIN;
-							break;
-					}
-				} else{
-					ymin = atof(*++argv);
-					argc--;
-					ymax = atof(*++argv);
-				}
-				break;
-			case 'h':
-			default:
-				usage();
-			}
-		} 
-		else 
-		    fp = getfp(*argv, "r");
+   if ((cmnd = strrchr(argv[0],'/'))==NULL)
+      cmnd = argv[0];
+   else
+      cmnd++;
+   while (--argc)
+      if (**++argv=='-') {
+         argc--;
+         switch (*(*argv+1)) {
+         case 'y':
+            if ((*(*argv+2))=='m') {
+               switch (*(*argv+3)) {
+               case 'i':
+                  ymin = atof(*++argv);
+                  ymax = FLT_MAX;
+                  break;
+               case 'a':
+                  ymax = atof(*++argv);
+                  ymin = FLT_MIN;
+                  break;
+               }
+            }
+            else {
+               ymin = atof(*++argv);
+               argc--;
+               ymax = atof(*++argv);
+            }
+            break;
+         case 'h':
+         default:
+            usage(1);
+         }
+      } 
+      else 
+         fp = getfp(*argv, "r");
 
-	x = dgetmem(2*256);
-	y = x + 256;
+   x = dgetmem(2*256);
+   y = x + 256;
 
-	while ((size = freadf(x, sizeof(*x), 256, fp)) !=0){
-		clip (x, size, ymin, ymax, y);
-		fwritef(y, sizeof(*y), size, stdout);
-	}
-	exit(0);
+   while ((size = freadf(x, sizeof(*x), 256, fp))!=0) {
+      clip (x, size, ymin, ymax, y);
+      fwritef(y, sizeof(*y), size, stdout);
+   }
+   
+   return 0;
 }
