951d3469e3504a90235720c19f24c6043e70d5ab
Heiga Zen <heigazen@users.sourceforge.net>
2007-07-24 05:06:37 +0000

    Cleanup source codes.  Add .cvsinore.

--- src/bin/fft/_fft.c
+++ src/bin/fft/_fft.c
@@ -59,117 +59,122 @@
 #include <SPTK.h>
 
 
-double	*_sintbl = 0;
-int 	maxfftsize = 0;
-int fft(double *x, double *y, int m)
+double *_sintbl=0;
+int maxfftsize=0;
+
+int fft (double *x, double *y, const int m)
 {
-	register int	j, lmx, li;
-	register double	*xp, *yp;
-	register double	*sinp,*cosp;
-	int	lf, lix,tblsize;
-	int	mv2, mm1;
-	double	t1, t2;
-	double  arg;
+   int j, lmx, li;
+   double *xp, *yp;
+   double *sinp,*cosp;
+   int lf, lix,tblsize;
+   int mv2, mm1;
+   double t1, t2;
+   double  arg;
+   int checkm(const int);
 
-	/**************
-	* RADIX-2 FFT *
-	**************/
+   /**************
+   * RADIX-2 FFT *
+   **************/
 
-	if(checkm(m))
-		return(-1);
+   if (checkm(m))
+      return(-1);
 
-	/***********************
-	* SIN table generation *
-	***********************/
+   /***********************
+   * SIN table generation *
+   ***********************/
 
-	if(_sintbl == 0 || maxfftsize < m ) {
-		tblsize=m-m/4+1;
-		arg=PI/m*2;
-		if (_sintbl != 0)
-			free(_sintbl);
-		_sintbl = sinp = dgetmem(tblsize);
-		*sinp++ = 0;
-		for(j = 1 ; j < tblsize ; j++)
-			*sinp++ = sin( arg * (double)j);
-		_sintbl[m/2] = 0;
-		maxfftsize = m;
-	}
+   if ((_sintbl==0) || (maxfftsize<m)) {
+      tblsize=m-m/4+1;
+      arg=PI/m*2;
+      if (_sintbl!=0)
+         free(_sintbl);
+      _sintbl = sinp = dgetmem(tblsize);
+      *sinp++ = 0;
+      for (j=1; j<tblsize; j++)
+         *sinp++ = sin(arg*(double)j);
+      _sintbl[m/2] = 0;
+      maxfftsize = m;
+   }
 
-	lf = maxfftsize / m;
-	lmx = m;
+   lf = maxfftsize / m;
+   lmx = m;
 
-	for(;;) {
-		lix = lmx;  
-		lmx /= 2;
-		if(lmx <= 1) break;
-		sinp = _sintbl;
-		cosp = _sintbl + maxfftsize/4;
-		for(j = 0 ; j < lmx ; j++) {
-			xp = &x[j];
-			yp = &y[j];
-			for(li = lix ; li <= m ; li += lix) {
-				t1 = *(xp) - *(xp + lmx);
-				t2 = *(yp) - *(yp + lmx);
-				*(xp) += *(xp + lmx);
-				*(yp) += *(yp + lmx);
-				*(xp + lmx) = *cosp * t1 + *sinp * t2;
-				*(yp + lmx) = *cosp * t2 - *sinp * t1;
-				xp += lix;
-				yp += lix;
-			}
-			sinp += lf;
-			cosp += lf;
-		}
-		lf += lf;
-	}
+   for(;;) {
+      lix = lmx;  
+      lmx /= 2;
+      if (lmx <= 1) break;
+      sinp = _sintbl;
+      cosp = _sintbl + maxfftsize/4;
+      for (j=0; j<lmx; j++) {
+         xp = &x[j];
+         yp = &y[j];
+         for (li=lix; li<=m ; li+=lix) {
+            t1 = *(xp) - *(xp + lmx);
+            t2 = *(yp) - *(yp + lmx);
+            *(xp) += *(xp + lmx);
+            *(yp) += *(yp + lmx);
+            *(xp + lmx) = *cosp * t1 + *sinp * t2;
+            *(yp + lmx) = *cosp * t2 - *sinp * t1;
+            xp += lix;
+            yp += lix;
+         }
+         sinp += lf;
+         cosp += lf;
+      }
+      lf += lf;
+   }
 
-	xp = x;
-	yp = y;
-	for(li = m / 2; li--; xp += 2, yp += 2) {
-		t1 = *(xp) - *(xp + 1);
-		t2 = *(yp) - *(yp + 1);
-		*(xp) += *(xp + 1);
-		*(yp) += *(yp + 1);
-		*(xp + 1) = t1;
-		*(yp + 1) = t2;
-	}
-	/***************
-	* bit reversal *
-	***************/
-	j = 0;
-	xp = x;
-	yp = y;
-	mv2 = m / 2;
-	mm1 = m - 1; 
-	for(lmx = 0 ; lmx < mm1 ; lmx++) {
-		if((li = lmx - j) < 0) {
-			t1 = *(xp);
-			t2 = *(yp);
-			*(xp) = *(xp + li);
-			*(yp) = *(yp + li);
-			*(xp + li) = t1;
-			*(yp + li) = t2;
-		}
-		li = mv2;
-		while(li <= j) {
-			j -= li;
-			li /= 2;
-		}
-		j += li;
-		xp = x + j;
-		yp = y + j;
-	}
-	return(0);
+   xp = x;
+   yp = y;
+   for (li=m/2; li--; xp+=2,yp+=2) {
+      t1 = *(xp) - *(xp + 1);
+      t2 = *(yp) - *(yp + 1);
+      *(xp) += *(xp + 1);
+      *(yp) += *(yp + 1);
+      *(xp + 1) = t1;
+      *(yp + 1) = t2;
+   }
+   
+   /***************
+   * bit reversal *
+   ***************/
+   j = 0;
+   xp = x;
+   yp = y;
+   mv2 = m / 2;
+   mm1 = m - 1; 
+   for (lmx=0; lmx<mm1; lmx++) {
+      if ((li=lmx-j)<0) {
+         t1 = *(xp);
+         t2 = *(yp);
+         *(xp) = *(xp + li);
+         *(yp) = *(yp + li);
+         *(xp + li) = t1;
+         *(yp + li) = t2;
+      }
+      li = mv2;
+      while (li<=j) {
+         j -= li;
+         li /= 2;
+      }
+      j += li;
+      xp = x + j;
+      yp = y + j;
+   }
+ 
+   return(0);
 }
 
-int checkm(int m)
+int checkm (const int m)
 {
-	register int	k;
+   int k;
 
-	for(k = 4; k <= m; k <<= 1 ) {
-		if(k == m)
-			return(0);
-	}
-	fprintf(stderr, "fft : m must be a integer of power of 2\n");
-	return(-1);
+   for (k=4; k<=m; k<<=1) {
+      if (k==m)
+         return(0);
+   }
+   fprintf(stderr, "fft : m must be a integer of power of 2\n");
+ 
+   return(-1);
 }
--- src/bin/fft/fft.c
+++ src/bin/fft/fft.c
@@ -63,141 +63,138 @@ static char *rcs_id = "$Id$";
 #include <stdio.h>
 #include <stdlib.h>
 #include <SPTK.h>
+#include <math.h>
 #include <string.h>
 
 
-/* Required Function */
-int dft(FILE *fp,int size,int nd,int out);
-
 /* Default Values */
-#define SIZE 	256
+#define SIZE 256
 
 
 /* Command Name */
-char 	*cmnd;
+char *cmnd;
 
 
-void usage(status)
-int status;
+void usage (int status)
 {
-	fprintf(stderr, "\n");
-	fprintf(stderr, " %s - FFT for complex sequence\n", cmnd);
-	fprintf(stderr, "\n");
-	fprintf(stderr, "  usage:\n");
-	fprintf(stderr, "       %s [ options ] [ infile ] > stdout\n", cmnd);
-	fprintf(stderr, "  options:\n");
-	fprintf(stderr, "       -l l  : FFT size  power of 2 [%d]\n", SIZE);
-	fprintf(stderr, "       -m m  : order of sequence    [l-1]\n");
-	fprintf(stderr, "       -A    : amplitude            [FALSE]\n");
-	fprintf(stderr, "       -R    : real part            [FALSE]\n");
-	fprintf(stderr, "       -I    : imaginary part       [FALSE]\n");
-	fprintf(stderr, "       -P    : power                [FALSE]\n");
-	fprintf(stderr, "       -h    : print this message\n");
-	fprintf(stderr, "  infile:\n");
-	fprintf(stderr, "       data sequence (float)        [stdin]\n");
-	fprintf(stderr, "  stdout:\n");
-	fprintf(stderr, "       FFT seqeunce (float)\n");
+   fprintf(stderr, "\n");
+   fprintf(stderr, " %s - FFT for complex sequence\n", cmnd);
+   fprintf(stderr, "\n");
+   fprintf(stderr, "  usage:\n");
+   fprintf(stderr, "       %s [ options ] [ infile ] > stdout\n", cmnd);
+   fprintf(stderr, "  options:\n");
+   fprintf(stderr, "       -l l  : FFT size  power of 2 [%d]\n", SIZE);
+   fprintf(stderr, "       -m m  : order of sequence    [l-1]\n");
+   fprintf(stderr, "       -A    : amplitude            [FALSE]\n");
+   fprintf(stderr, "       -R    : real part            [FALSE]\n");
+   fprintf(stderr, "       -I    : imaginary part       [FALSE]\n");
+   fprintf(stderr, "       -P    : power                [FALSE]\n");
+   fprintf(stderr, "       -h    : print this message\n");
+   fprintf(stderr, "  infile:\n");
+   fprintf(stderr, "       data sequence (float)        [stdin]\n");
+   fprintf(stderr, "  stdout:\n");
+   fprintf(stderr, "       FFT seqeunce (float)\n");
 #ifdef SPTK_VERSION
-	fprintf(stderr, "\n");
-	fprintf(stderr, " SPTK: version %s",SPTK_VERSION);
+   fprintf(stderr, "\n");
+   fprintf(stderr, " SPTK: version %s\n", SPTK_VERSION);
+   fprintf(stderr, " CVS Info: %s", rcs_id);
 #endif
-	fprintf(stderr, "\n");
-	exit(status);
+   fprintf(stderr, "\n");
+   exit(status);
 }
 
 
-int main(int argc,char *argv[])
+int main (int argc,char *argv[])
 {
-	FILE	*fp;
-	char	*s, *infile = NULL, c;
-	int	size = SIZE, nd = -1, out = ' ';
+   FILE *fp;
+   char *s, *infile=NULL, c;
+   int size=SIZE, nd=-1, out=' ';
+   int dft (FILE *fp, const int size, const int nd, const int out);
+   
+   if ((cmnd = strrchr(argv[0], '/'))==NULL)
+      cmnd = argv[0];
+   else
+      cmnd++;
 
-        if ((cmnd = strrchr(argv[0], '/')) == NULL)
-	    cmnd = argv[0];
-        else
-	    cmnd++;
-
-	while(--argc) {
-		if(*(s = *++argv) == '-') {
-			c = *++s;
-			switch(c) {
-			case 'l':
-				size = atoi(*++argv);
-				argc--;
-				break;
-			case 'm':
-				nd = atoi(*++argv) + 1;
-				argc--;
-				break;
-			case 'i':
-			case 'p':
-			case 'r':
-				c -= ('a' - 'A');
-#ifdef	AOP
-			case 'A':
+   while (--argc) {
+      if (*(s = *++argv)=='-') {
+         c = *++s;
+         switch(c) {
+         case 'l':
+            size = atoi(*++argv);
+            argc--;
+            break;
+         case 'm':
+            nd = atoi(*++argv) + 1;
+            argc--;
+            break;
+         case 'i':
+         case 'p':
+         case 'r':
+            c -= ('a' - 'A');
+#ifdef AOP
+         case 'A':
 #endif
-			case 'I':
-			case 'P':
-			case 'R':
-				out = c;
-				break;
-			case 'h':
-				usage(0);
-			default:
-				fprintf(stderr,
-					"%s: unknown option '%c'\n", cmnd, c);
-				usage(1);
-			}
-		}
-		else
-			infile = s;
-	}
+         case 'I':
+         case 'P':
+         case 'R':
+            out = c;
+            break;
+         case 'h':
+            usage(0);
+         default:
+            fprintf(stderr,"%s: unknown option '%c'\n", cmnd, c);
+            usage(1);
+         }
+      }
+      else
+         infile = s;
+   }
 
-	if(nd == -1) nd = size; 
-	if(nd > size) {
-		fprintf(stderr, "%s: oder of sequence > FFT size\n", cmnd);
-		exit(1);
-	}
-	if(infile) {
-		fp = getfp(infile, "r");
-		dft(fp,size,nd,out);
-		fclose(fp);
-	}
-	else
-		dft(stdin,size,nd,out);
-	exit(0);
+   if (nd==-1) nd = size; 
+   if (nd>size) {
+      fprintf(stderr, "%s: oder of sequence > FFT size\n", cmnd);
+      exit(1);
+   }
+   if (infile) {
+      fp = getfp(infile, "r");
+      dft(fp,size,nd,out);
+      fclose(fp);
+   }
+   else
+      dft(stdin,size,nd,out);
+   
+   return 0;
 }
 
-int dft(FILE *fp,int size,int nd,int out)
+int dft (FILE *fp, const int size, const int nd, const int out)
 {
-	double	*x, *y;
-	register int	k ,size2;
-#ifdef	AOP
-	double	sqrt();
-#endif
+   double *x, *y;
+   int k ,size2;
 
-	x = dgetmem(size2 = size + size);
-	y = x + size;
+   x = dgetmem(size2=size+size);
+   y = x + size;
 
-	while(!feof(fp)) {
-		fillz(x, size2, sizeof(double));
-		if(freadf(x, sizeof(*x), nd, fp) == 0)
-			break;
-		if(freadf(y, sizeof(*y), nd, fp) == 0)
-			break;
-		fft(x, y, size);
-		if(out == 'P')
-			for(k = 0; k < size; ++k)
-				x[k] = x[k] * x[k] + y[k] * y[k];
-#ifdef	AOP
-		else if(out == 'A')
-			for(k = 0; k < size; ++k)
-				x[k] = sqrt(x[k] * x[k] + y[k] * y[k]);
+   while (!feof(fp)) {
+      fillz(x, size2, sizeof(double));
+      if (freadf(x, sizeof(*x), nd, fp)==0)
+         break;
+      if (freadf(y, sizeof(*y), nd, fp)==0)
+         break;
+      fft(x, y, size);
+      if (out=='P')
+         for (k=0; k<size; k++)
+            x[k] = x[k] * x[k] + y[k] * y[k];
+#ifdef AOP
+      else if (out=='A')
+         for (k=0; k<size; k++)
+            x[k] = sqrt(x[k]*x[k] + y[k]*y[k]);
 #endif
-		if(out != 'I')
-			fwritef(x, sizeof(*x), size, stdout);
-		if(out == ' ' || out == 'I')
-			fwritef(y, sizeof(*y), size, stdout);
-	}
-	return(0);
+      if (out!='I')
+         fwritef(x, sizeof(*x), size, stdout);
+      if (out==' ' || out=='I')
+         fwritef(y, sizeof(*y), size, stdout);
+   }
+   
+   return(0);
 }
