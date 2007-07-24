9d89e254e5989c3523e8caf7b2505e371543b146
Heiga Zen <heigazen@users.sourceforge.net>
2007-07-24 04:51:04 +0000

    Cleanup source codes.  Add .cvsinore.

--- src/bin/fd/fd.c
+++ src/bin/fd/fd.c
@@ -45,7 +45,7 @@
 *					1996.5  K.Koishida		*
 *									*
 *	usage:								*
-*		fdmp [options] [infile] > stdout			*
+*		fd [options] [infile] > stdout			*
 *	options:							*
 *		-a a     :  address				[0]	*
 *		-n n     :  initial value for numbering         [0]	*
@@ -68,200 +68,205 @@ static char *rcs_id = "$Id$";
 #include <ctype.h>
 #include <SPTK.h>
 
-/*  Required Functions  */
-void fdump(FILE *fp);
 
 /*  Default Values  */
-#define START		0
-#define MODULO		0x7fffffff
-#define ENTRY		0
-#define SIZE            128
+#define START 0
+#define MODULO 0x7fffffff
+#define ENTRY 0
+#define SIZE 128
 
 
 /*  Command Name  */
-char	*cmnd;
+char *cmnd;
 
 
-void usage(int status)
+void usage (int status)
 {
-    fprintf(stderr, "\n");
-    fprintf(stderr, " %s - file dump\n",cmnd);
-    fprintf(stderr, "\n");
-    fprintf(stderr, "  usage:\n");
-    fprintf(stderr, "       %s [ options ] [ infile ] > stdout\n", cmnd);
-    fprintf(stderr, "  options:\n");
-    fprintf(stderr, "       -a a  : address                     [%d]\n",START);
-    fprintf(stderr, "       -n n  : initial value for numbering [%d]\n",START);
-    fprintf(stderr, "       -m m  : modulo for numbering        [EOF]\n");
-    fprintf(stderr, "       -ent  : number of data in each line [%d]\n",ENTRY);
-    fprintf(stderr, "       +type : data type                   [c]\n");
-    fprintf(stderr, "                c (char)      s (short)\n");
-    fprintf(stderr, "                i (int)       l (long)\n");
-    fprintf(stderr, "                f (float)     d (double)\n");
-    fprintf(stderr, "       %%form : print format(printf style) [N/A]\n");
-    fprintf(stderr, "       -h    : print this message\n");
-    fprintf(stderr, "  infile:\n");
-    fprintf(stderr, "       data sequence                       [stdin]\n");
-    fprintf(stderr, "  stdout:\n");
-    fprintf(stderr, "       dumped data sequence\n");
+   fprintf(stderr, "\n");
+   fprintf(stderr, " %s - file dump\n",cmnd);
+   fprintf(stderr, "\n");
+   fprintf(stderr, "  usage:\n");
+   fprintf(stderr, "       %s [ options ] [ infile ] > stdout\n", cmnd);
+   fprintf(stderr, "  options:\n");
+   fprintf(stderr, "       -a a  : address                     [%d]\n",START);
+   fprintf(stderr, "       -n n  : initial value for numbering [%d]\n",START);
+   fprintf(stderr, "       -m m  : modulo for numbering        [EOF]\n");
+   fprintf(stderr, "       -ent  : number of data in each line [%d]\n",ENTRY);
+   fprintf(stderr, "       +type : data type                   [c]\n");
+   fprintf(stderr, "                c (char)      s (short)\n");
+   fprintf(stderr, "                i (int)       l (long)\n");
+   fprintf(stderr, "                f (float)     d (double)\n");
+   fprintf(stderr, "       %%form : print format(printf style) [N/A]\n");
+   fprintf(stderr, "       -h    : print this message\n");
+   fprintf(stderr, "  infile:\n");
+   fprintf(stderr, "       data sequence                       [stdin]\n");
+   fprintf(stderr, "  stdout:\n");
+   fprintf(stderr, "       dumped data sequence\n");
 #ifdef SPTK_VERSION
-    fprintf(stderr, "\n");
-    fprintf(stderr, " SPTK: version %s",SPTK_VERSION);
+   fprintf(stderr, "\n");
+   fprintf(stderr, " SPTK: version %s\n", SPTK_VERSION);
+   fprintf(stderr, " CVS Info: %s", rcs_id);
 #endif
-    fprintf(stderr, "\n");
-    exit(status);
+   fprintf(stderr, "\n");
+   exit(status);
 }
 
 
-long	start = START, mod = MODULO;
-int	size = sizeof(char), is_int = 0, entry = ENTRY, is_char = 1, ff = 0;
-char	adrsfmt = 'd', format[SIZE], form[SIZE];
+long start=START, mod=MODULO;
+int size=sizeof(char), is_int=0, entry=ENTRY, is_char=1, ff=0;
+char adrsfmt='d', format[SIZE], form[SIZE];
 
 
-int main(int argc, char **argv)
+int main (int argc, char **argv)
 {
-    FILE 	   *fp = stdin;
-    char  *s, c;
-    long	   atol();
+   FILE *fp=stdin;
+   char *s, c;
+   void fdump(FILE *fp);
     
-    if ((cmnd = strrchr(argv[0], '/')) == NULL)
-	cmnd = argv[0];
-    else
-	cmnd++;
-    while (--argc)
-	if(*(s = *++argv) == '-') {
-	    if((c = *++s) > '0' && c <= '9')
-		c = 'c';
-	    switch(c) {
-		case 'a':
-		case 'n':
-		    adrsfmt = c;
-		    start = atol(*++argv);
-		    --argc;
-		    break;
-		case 'm':
-		    mod = atol(*++argv);
-		    --argc;
-		    break;
-		case 'c':
-		    entry = atoi(s);
-		    break;
-		case 'h':
-		    usage(0);
-		default:
-		    fprintf(stderr, "%s : Invalid option '%c' !\n", cmnd, *(*argv+1));
-		    usage(1);
-		}
-	}
-	else if (*s == '+') {
-	    c = *++s;
-	    switch(c) {
-		case 'b':
-		    is_char = 0;
-		    break;
-		case 'c':
-		    size = sizeof(char);
-		    break;
-		case 's':
-		    size = sizeof(short);
-		    break;
-		case 'l':
-		case 'i':
-		    is_int = 1;
-		case 'f':
-		    size = sizeof(float);
-		    break;
-		case 'd':
-		    size = sizeof(double);
-		    break;
-		default:
-		    fprintf(stderr, "%s : Invalid option '%c' !\n", cmnd, *(*argv+1));
-		    usage(1);
-	    }
-	}
-        else if( *s == '%'){
-		strcpy(format,s);
-		ff = 1;
-	}
-	else
-	    fp = getfp(*argv, "r");
+   if ((cmnd = strrchr(argv[0], '/'))==NULL)
+      cmnd = argv[0];
+   else
+      cmnd++;
+   
+   while (--argc)
+      if (*(s = *++argv)=='-') {
+         if (((c=*++s)>'0') && (c<='9'))
+            c = 'c';
+         switch (c) {
+         case 'a':
+         case 'n':
+            adrsfmt = c;
+            start = atol(*++argv);
+            --argc;
+            break;
+         case 'm':
+            mod = atol(*++argv);
+            --argc;
+            break;
+         case 'c':
+            entry = atoi(s);
+            break;
+         case 'h':
+            usage(0);
+         default:
+            fprintf(stderr, "%s : Invalid option '%c' !\n", cmnd, *(*argv+1));
+            usage(1);
+         }
+      }
+      else if (*s=='+') {
+         c = *++s;
+         switch (c) {
+         case 'b':
+            is_char = 0;
+            break;
+         case 'c':
+            size = sizeof(char);
+            break;
+         case 's':
+            size = sizeof(short);
+            break;
+         case 'l':
+            size = sizeof(long);
+            is_int = 1;
+            break;
+         case 'i':
+            size = sizeof(int);
+            is_int = 1;
+            break;
+         case 'f':
+            size = sizeof(float);
+            break;
+         case 'd':
+            size = sizeof(double);
+            break;
+         default:
+            fprintf(stderr, "%s : Invalid option '%c' !\n", cmnd, *(*argv+1));
+            usage(1);
+         }
+      }
+      else if (*s=='%') {
+         strcpy(format,s);
+         ff = 1;
+      }
+      else
+         fp = getfp(*argv, "r");
 
-    fdump(fp);
+   fdump(fp);
 
-    exit(0);
+   return 0;
 }
 
-void fdump(FILE *fp)
+void fdump (FILE *fp)
 {
-    char s[18];
-    long	adrs, i, n;
-    union {
-	unsigned char	b;
-	short	w;
-	long	l;
-	float	f;
-	double	d;
-    }u;
+   char s[18];
+   long adrs, i, n;
+   union {
+      unsigned char b;
+      short w;
+      long l;
+      float f;
+      double d;
+   } u;
 
-    if(ff) strcat(format," ");
+   if (ff) strcat(format," ");
     
-    if(entry == 0) {
-	if(size == 8)
-	    entry = 2;
-	else if(size > 1)
-	    entry = 5;
-	else if(is_char)
-	    entry = 16;
-	else
-	    entry = 10;
-    }
+   if (entry==0) {
+      if (size==8)
+         entry = 2;
+      else if (size>1)
+         entry = 5;
+      else if (is_char)
+         entry = 16;
+      else
+         entry = 10;
+   }
     
-    fread(&u, size, 1, fp);
-    while(!feof(fp))
-	for(n = adrs = start;n < mod && !feof(fp);adrs += entry * size){
-	    if(adrsfmt == 'a')
-		printf("%06lx  ", adrs);
-	    else if(adrsfmt == 'n')
-		printf("%7ld  ", n);
+   fread(&u, size, 1, fp);
+   
+   while(!feof(fp))
+      for (n=adrs=start; (n<mod) && !feof(fp); adrs+=entry*size) {
+         if (adrsfmt=='a')
+            printf("%06lx  ", adrs);
+         else if (adrsfmt=='n')
+            printf("%7ld  ", n);
 
-	    for(i = 0; i < entry && !feof(fp) && n < mod; ++i, ++n){
-		switch(size) {
-		    case 1:
-		        s[i] = ((u.b & 0x7f) < 32 || u.b == 0x7f || u.b >= 0xe0) ? '.' : u.b;
-			if(is_char)
-			    printf("%02x ", u.b);
-			else
-			    printf("%5d", u.b);
-			break;
-		    case 2:
-			printf("%7d", u.w);
-			break;
-		    case 4:
-			if(is_int)
-			    printf("%ld", u.l);
-			else if(ff)
-			    printf(format, u.f);
-			else
-			    printf("%14.6e", u.f);
-			break;
-		    case 8:
-			if(ff)
-			    printf(format, u.d);
-			else
-			    printf("%24.15e", u.d);
-		    default:
-			break;
-		}
-		fread(&u, size, 1, fp);
-	    }
-	    if(size == 1 && is_char) {
-		s[i] = '\0';
-		while(i++ < entry)
-		    printf("   ");
-		printf("|%s|\n", s);
-	    }
-	    else
-		printf("\n");
-	}
+         for (i=0; (i<entry) && !feof(fp) && (n<mod); i++,n++) {
+            switch (size) {
+            case 1:
+               s[i] = (((u.b&0x7f)<32) || (u.b==0x7f) || (u.b>=0xe0)) ? '.' : u.b;
+               if (is_char)
+                  printf("%02x ", u.b);
+               else
+                  printf("%5d", u.b);
+               break;
+            case 2:
+               printf("%7d", u.w);
+               break;
+            case 4:
+               if (is_int)
+                  printf("%ld", u.l);
+               else if (ff)
+                  printf(format, u.f);
+               else
+                  printf("%14.6e", u.f);
+               break;
+            case 8:
+               if (ff)
+                  printf(format, u.d);
+               else
+                  printf("%24.15e", u.d);
+            default:
+               break;
+            }
+            fread(&u, size, 1, fp);
+         }
+         if (size==1 && is_char) {
+            s[i] = '\0';
+            while (i++<entry)
+               printf("   ");
+            printf("|%s|\n", s);
+         }
+         else
+            printf("\n");
+      }
 }
-
