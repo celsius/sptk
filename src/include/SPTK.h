f77927a418bca0020ec1ed20897c26a74781d099
Heiga Zen <heigazen@users.sourceforge.net>
2007-07-25 05:36:41 +0000

    List proto-types of SPTK functions in libSPTK.a (from acep to ifft).

--- src/include/SPTK.h
+++ src/include/SPTK.h
@@ -47,6 +47,13 @@
 #define PI2 6.28318530717958647692
 #endif
 
+#ifndef M_PI
+#define M_PI  3.1415926535897932384626433832795
+#define M_2PI 6.2831853071795864769252867665590
+#endif
+
+#define  LN_TO_LOG   4.3429448190325182765
+
 /* #ifndef ABS(x) */
 #define ABS(x) (x < 0. ? -x : x)
 /* #endif */
@@ -60,25 +67,68 @@ typedef enum _Window {
    RECTANGULAR
 } Window;
 
-void fillz(char *, int, int);
-void movem(char *, char *, int, int);
-short *sgetmem(int);
-long *lgetmem(int);
-float *fgetmem(int);
-float **ffgetmem(int);
+typedef enum _Boolean {FA, TR} Boolean;
+char *BOOL[] = {"FALSE", "TRUE"};
+
+
+/* library routines */
+double agexp (double, double, double);
+int cholesky (double *, double *, double *, const int, double);
+void fillz (void *, int, int);
+FILE *getfp (char *, char *);
+short *sgetmem (const int);
+long *lgetmem (const int);
+float *fgetmem (const int);
+float **ffgetmem (const int);
 /*real   *rgetmem();*/
-double *dgetmem(int);
-char *getmem(int, unsigned int);
-
-FILE *getfp(char *, char *);
-
+double *dgetmem (const int);
+char *getmem (const int, const unsigned int);
+double gexp (const double, const double);
+double glog (const double, const double);
+int ifftr (double *, double *, const int);
+void multim (double *, const int, const int, double *, const int, const int, double *);
+void addm (double *, double *, const int, const int, double *);
+void movem (void *, void *, const int, const int);
+int mseq (void);
+int theq (double *, double *, double *, double *, const int, double);
+int toeplitz (double *, double *, double *, const int, double);
 #ifdef DOUBLE
 #define fwritef fwrite
 #define freadf  fread
 #else /* DOUBLE */
-int fwritef();
-int freadf();
+int fwritef (double *, const unsigned int, const int, FILE *);
+int freadf  (double *, const unsigned int, const int, FILE *);
 #endif /* DOUBLE */
 
-int theq(double *, double *, double *, double *, int, double);
+/* tool routines */
+double acep (double x, double *c, const int m, const double lambda, const double step, const double tau, const int pd, const double eps);
+void acorr (double *x, int l, double *r, const int np);
+double agcep (double x, double *c, const int m, const int stage, const double lambda, const double step, const double tau, const double eps);
+double amcep (double x, double *b, const int m, const double a, const double lambda, const double step, const double tau, const int pd, const double eps);
+void phidf (const double x, const int m, double a, double *d);
+double average (double *x, const int n);
+void vaverage (double *x, const int l, const int num, double *ave);
+void b2mc (double *b, double *mc, int m, const double a);
+void c2acr(double *c, const int m1, double *r, const int m2, const int flng);
+void c2ir (double *c, const int nc, double *h, const int leng);
+void ic2ir (double *h, const int leng, double *c, const int nc);
+void c2sp (double *c, const int m, double *x, double *y, const int l);
+void clip (double *x, const int l, const double min, const double max, double *y);
+double df2 (const double x, const double sf, const double f0p, const double wbp, const double f0z, const double wbz, const int fp, const int fz, double *buf, int *bufp);
+double dfs (double x, double *a, int m, double *b, int n, double *buf, int *bufp);
+int fft (double *x, double *y, const int m);
+int fft2 (double x[], double y[], const int n);
+void fftcep (double *sp, const int flng, double *c, const int m, int itr, double ac);
+int fftr (double *x, double *y, const int m);
+int fftr2 (double x[], double y[], const int n);
+void freqt (double *c1, const int m1, double *c2, const int m2, const double a);
+void gc2gc (double *c1, const int m1, const double g1, double *c2, const int m2, const double g2);
+int gcep (double *xw, const int flng, double *gc, const int m, const double g, const int itr1, const int itr2, const double d, const double e);
+double glsadf (double x, double *c, const int m, const int n, double *d);
+double glsadf1 (double x, double *c, const int m, const int n, double *d);
+double gpoledf (double x, double *c, int m, const double g, double *d);
+void gnorm (double *c1, double *c2, int m, const double g);
+void grpdelay (double *x, double *gd, const int size, const int is_arma);
+int histogram (double *x, const int size, const double min, const double max, const double step, double *h);
+int ifft (double *x, double *y, const int m);
 
