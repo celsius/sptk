3bba31745f94c1adee90d70891ad918434915f60
Heiga Zen <heigazen@users.sourceforge.net>
2007-08-07 05:42:01 +0000

    Add CVS Id.  Remove prototype definitions for functions in stdlib.h and math.h.

--- src/bin/fig+fdrw/plot.h
+++ src/bin/fig+fdrw/plot.h
@@ -1,15 +1,15 @@
 /*
   ----------------------------------------------------------------
-	Speech Signal Processing Toolkit (SPTK): version 3.0
-			 SPTK Working Group
+ Speech Signal Processing Toolkit (SPTK): version 3.0
+    SPTK Working Group
 
-		   Department of Computer Science
-		   Nagoya Institute of Technology
-				and
+     Department of Computer Science
+     Nagoya Institute of Technology
+    and
     Interdisciplinary Graduate School of Science and Engineering
-		   Tokyo Institute of Technology
-		      Copyright (c) 1984-2000
-			All Rights Reserved.
+     Tokyo Institute of Technology
+        Copyright (c) 1984-2000
+   All Rights Reserved.
 
   Permission is hereby granted, free of charge, to use and
   distribute this software and its documentation without
@@ -38,80 +38,76 @@
 */
 
 /****************************************************************
-*	HEADER for XY-PLOTTER library				*
-*								*
-*	Following commands are valid for the xy plotter FP5301	*
-*								*
-*						Oct. 1, 1985	*
+* $Id$          *  
+* HEADER for XY-PLOTTER library    *
+*        *
+* Following commands are valid for the xy plotter FP5301 *
+*        *
+*      Oct. 1, 1985 *
 ****************************************************************/
 
 #include <stdio.h>
 
 /* commands for FP5301 */
 
-#define	TRM		0				/* terminator */
+#define TRM 0    /* terminator */
 
-#define	_speed(l, n)	printf("!%d %d%c", l, n, TRM)	/* speed */
-#define	_speed_all(l)	printf("!%d%c", l, TRM)		/* speed */
-#define	ERRMASK	0x8022			/* error mask		*/
-#define	_font(n)	printf("$%d%c", n, TRM)		/* font	*/
-#define	_hatch(m,p,t)	printf("%%%d %d %d\n", m, p, t)	/* hatching */
-#define _factor(p,q,r)	printf("&%d %d %d\n", p, q, r)	/* factor */
-#define	ELLIPS	0x8029			/* ellips		*/
-#define	_rotate(x,y,t)	printf("/%d %d %d\n", x, y, t)	/* rotate */
-#define	clear()		putchar(':')			/* clear */
-#define	ifclear()	putchar(';')			/* interface clear */
-#define	term(t)		printf("=%c%c", t, t)		/* term */
-#define	CLIP	0x803e			/* clipping		*/
-#define	RDOFF	0x803f			/* read offset		*/
-#define ARESET	0x8041			/* alpha reset		*/
-#define line_scale(l)	printf("B%d\n", l)		/* line scale */
-#define draw(x, y)	printf("D%d %d\n%c", x, y, TRM)	/* draw	*/
-#define _draw()		putchar('D')			/* draw	command */
-#define rdraw(x, y)	printf("E%d %d\n%c", x, y, TRM)	/* relative draw */
-#define	home();		putchar('H')			/* home	*/
-#define	aitalic(p)	printf("I%d\n", p)		/* alpha italic	*/
-#define newpen(p)	printf("J%d\n", p)		/* new pen */
-#define KANA	0x804b			/* kana			*/
-#define line_type(p)	printf("L%d\n", p)		/* line type */
-#define	move(x, y)	printf("M%d %d\n", x, y)	/* move	*/
-#define _mark(m)	printf("N%d\n", m)		/* mark	*/
-#define rmove(x, y)	printf("O%d %d\n", x, y)	/* relative move */
-#define print(p)	printf("P%s%c", p, TRM)		/* print */
-#define aspace(l)	printf("Q%d\n", l)		/* alpha space */
-#define arotate(t)	printf("R%d\n", t)		/* alpha rotate	*/
-#define ascale(a)	printf("S%d\n", a)		/* alpha scale */
-#define	PROMPT	0x8054			/* prompt light		*/
-#define	_circle(x,y,q,r,s,t)	printf("W%d %d %d %d %d %d%c",x,y,q,r,s,t,TRM)
-							/* circle */
-#define	_axis(p,q,r,s,t)	printf("X%d %d %d %d %d%c", p,q,r,s,t,TRM)
-							/* axis	*/
-#define	_curve(a)	printf("Y%d\n", a)		/* curve */
-#define	wup_left(x, y)	printf("Z%d %d\n", x, y)	/* write upper right */
-#define	wlo_right(x, y)	printf("\\%d %d\n", x, y)	/* write lower left */
-#define	_rcircle(q,r,s,t)	printf("]%d %d %d %d%c",q,r,s,t,TRM)
-							/* relative circle */
-#define	_offset(x, y)	printf("^%d %d\n", x, y)	/* offset */
-#define	RCURVE	0x805f			/* relative curve	*/
-#define	asciimode()	printf("%c%c", 0x80, 0)		/* binary terminator */
-#define	terminate()	putchar(TRM)			/* terminator */
-#define	sndcord(x, y)	printf("%d %d\n", x, y)		/* send (x, y) */
+#define _speed(l, n) printf("!%d %d%c", l, n, TRM) /* speed */
+#define _speed_all(l) printf("!%d%c", l, TRM)  /* speed */
+#define ERRMASK 0x8022   /* error mask  */
+#define _font(n) printf("$%d%c", n, TRM)  /* font */
+#define _hatch(m,p,t) printf("%%%d %d %d\n", m, p, t) /* hatching */
+#define _factor(p,q,r) printf("&%d %d %d\n", p, q, r) /* factor */
+#define ELLIPS 0x8029   /* ellips  */
+#define _rotate(x,y,t) printf("/%d %d %d\n", x, y, t) /* rotate */
+#define clear()  putchar(':')   /* clear */
+#define ifclear() putchar(';')   /* interface clear */
+#define term(t)  printf("=%c%c", t, t)  /* term */
+#define CLIP 0x803e   /* clipping  */
+#define RDOFF 0x803f   /* read offset  */
+#define ARESET 0x8041   /* alpha reset  */
+#define line_scale(l) printf("B%d\n", l)  /* line scale */
+#define draw(x, y) printf("D%d %d\n%c", x, y, TRM) /* draw */
+#define _draw()  putchar('D')   /* draw command */
+#define rdraw(x, y) printf("E%d %d\n%c", x, y, TRM) /* relative draw */
+#define home();  putchar('H')   /* home */
+#define aitalic(p) printf("I%d\n", p)  /* alpha italic */
+#define newpen(p) printf("J%d\n", p)  /* new pen */
+#define KANA 0x804b   /* kana   */
+#define line_type(p) printf("L%d\n", p)  /* line type */
+#define move(x, y) printf("M%d %d\n", x, y) /* move */
+#define _mark(m) printf("N%d\n", m)  /* mark */
+#define rmove(x, y) printf("O%d %d\n", x, y) /* relative move */
+#define print(p) printf("P%s%c", p, TRM)  /* print */
+#define aspace(l) printf("Q%d\n", l)  /* alpha space */
+#define arotate(t) printf("R%d\n", t)  /* alpha rotate */
+#define ascale(a) printf("S%d\n", a)  /* alpha scale */
+#define PROMPT 0x8054   /* prompt light  */
+#define _circle(x,y,q,r,s,t) printf("W%d %d %d %d %d %d%c",x,y,q,r,s,t,TRM)
+/* circle */
+#define _axis(p,q,r,s,t) printf("X%d %d %d %d %d%c", p,q,r,s,t,TRM)
+/* axis */
+#define _curve(a) printf("Y%d\n", a)  /* curve */
+#define wup_left(x, y) printf("Z%d %d\n", x, y) /* write upper right */
+#define wlo_right(x, y) printf("\\%d %d\n", x, y) /* write lower left */
+#define _rcircle(q,r,s,t) printf("]%d %d %d %d%c",q,r,s,t,TRM)
+/* relative circle */
+#define _offset(x, y) printf("^%d %d\n", x, y) /* offset */
+#define RCURVE 0x805f   /* relative curve */
+#define asciimode() printf("%c%c", 0x80, 0)  /* binary terminator */
+#define terminate() putchar(TRM)   /* terminator */
+#define sndcord(x, y) printf("%d %d\n", x, y)  /* send (x, y) */
 
 /* interfacing */
 
 struct co_ord {
-	float	xo;
-	float	yo;
-	};
+   float xo;
+   float yo;
+};
 
-#define sign(x)		(x & 0xffff8000)
-
-double	sin(), cos(), tan();
-int abs();
-void sleep();
-#define	DEG_RAD		(3.141592653589793 / 180.0)
-
-#define SCALE	4096
+#define sign(x)  (x & 0xffff8000)
+#define DEG_RAD  (3.141592653589793 / 180.0)
+#define SCALE 4096
 
 /*  Required Functions  */
 void rotate(float th);
@@ -119,23 +115,23 @@ void factor(float fx, float fy);
 int offset(float x, float y);
 void bound(double xl, double yl, double xh, double yh);
 void rstbnd();
-int hatch(int ip, register float *ax, register float *ay, int n, float d, float t);
+int hatch(int ip, float *ax, float *ay, int n, float d, float t);
 int pen(int pen);
 int font(int n);
 int speed(int isp, int ip);
-/*int mark(int mrk, float ax[], float ay[], int n, float f, int m);*/
+int mark(int mrk, float ax[], float ay[], int n, float f, int m);
 int symbol(float x, float y, char *text, float h, float s, float th);
 int number(float x, float y, float fval, float h, float s, float th, int m, int n);
 int italic(float th);
-int line(int ip, register float *ax, register float *ay, int n);
+int line(int ip, float *ax, float *ay, int n);
 int circle(float x, float y, float rs, float re, float ths, float the);
-int pntstyl(int ip);	
+int pntstyl(int ip);
 void plots(int mode);
 void plote();
 int _plnorm(float x);
 int _cordx(float x);
 int _cordy(float y);
-void plot(float x,float y, register int z);
+void plot(float x,float y, int z);
 int plotr(int ip, float x, float y);
 int plota(int ip, float x, float y);
 void mode(int ltype, float lscale);
@@ -147,15 +143,15 @@ int flush(float x[],float y[],int n);
 void polyg(float x[],float y[],int n);
 int is_in(float x,float y);
 void swap(float *x,float *y);
-char    *gettxt(register char *s);
-char    *script(register char *s, register int i);
-char    *getarg(register char *s, register char *arg);
-char    *gettyp(register char *s, register char *t);
-char    *getname(register char *s, register char *t);
+char *gettxt(char *s);
+char *script(char *s, int i);
+char *getarg(char *s, char *arg);
+char *gettyp(char *s, char *t);
+char *getname(char *s, char *t);
 int greek(char *p);
-float   sleng(char *p, float h, float w);
+float sleng(char *p, float h, float w);
 void _symbol(float x, float y, char *p, float h, float w, float t);
-float   ysadj();
-float   rx(float x, float y, float t);
-float   ry(float x, float y, float t);
-float   argapf(float x, float a);
+float ysadj(void);
+float rx(float x, float y, float t);
+float ry(float x, float y, float t);
+float argapf(float x, float a);
