80013233861244b5bb0b5530178f1f20e0f16f92
Heiga Zen <heigazen@users.sourceforge.net>
2007-09-15 14:41:36 +0000

    uscd -> us16

--- src/bin/us/us16.sh
+++ src/bin/us/us16.sh
@@ -168,7 +168,7 @@ usage:
 
 cat2 <<EOF
 
- uscd - sampling rate conversion from 10|12 kHz to 16 kHz
+ us16 - sampling rate conversion from 10|12 kHz to 16 kHz
 
   usage:
        $cmnd [ options ] [ infile ] [ outfile ]
