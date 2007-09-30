6c129fc6c2cea31e889ebd94e6e8dde88a479182
heigazen <heigazen>
2007-09-30 07:34:15 +0000

    Check rmd using isnan().

--- src/bin/levdur/_levdur.c
+++ src/bin/levdur/_levdur.c
@@ -49,7 +49,7 @@
 
 /****************************************************************
 
-    $Id: _levdur.c,v 1.10 2007/09/20 16:29:45 heigazen Exp $
+    $Id: _levdur.c,v 1.11 2007/09/30 07:34:15 heigazen Exp $
 
     Solve an Autocorrelation Normal Equation
     Using Levinson-Durbin Method
@@ -92,7 +92,7 @@ int levdur (double *r, double *a, const int m, double eps)
 
    if (eps<0.0) eps = 1.0e-6;
    rmd=r[0];
-   if (((rmd < 0.0) ? -rmd : rmd) <= eps) return(-1);   
+   if ( (((rmd<0.0)?-rmd:rmd)<=eps) || isnan(rmd) ) return(-1);
    a[0] = 0.0;
 
    for (l=1; l<=m; l++) {
@@ -106,7 +106,7 @@ int levdur (double *r, double *a, const int m, double eps)
       a[l] = mue;
 
       rmd = (1.0 - mue * mue) * rmd;
-      if (((rmd<0.0) ? -rmd : rmd) <= eps) return(-1);
+      if ( (((rmd<0.0)?-rmd:rmd)<=eps) || isnan(rmd) ) return(-1);
       if (((mue<0.0) ? -mue : mue) >= 1.0) flag = -2;
 
       for (k=0; k<=l; k++) c[k] = a[k];
