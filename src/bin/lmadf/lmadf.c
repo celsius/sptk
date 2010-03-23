a9c57ae95f31e1b737fc95cfbe0efebe4ddc7e7f
tatsuyaito <tatsuyaito>
2010-03-23 06:31:56 +0000

    Mar.23.2010 transpose filter for mlsadf lmadf updated

--- src/bin/lmadf/lmadf.c
+++ src/bin/lmadf/lmadf.c
@@ -8,7 +8,7 @@
 /*                           Interdisciplinary Graduate School of    */
 /*                           Science and Engineering                 */
 /*                                                                   */
-/*                1996-2009  Nagoya Institute of Technology          */
+/*                1996-2010  Nagoya Institute of Technology          */
 /*                           Department of Computer Science          */
 /*                                                                   */
 /* All rights reserved.                                              */
@@ -58,6 +58,7 @@
 *               -P P     :  order of Pade approximation   [4]           *
 *               -k       :  filtering without gain        [FALSE]       *
 *               -v       :  inverse filter                [FALSE]       *
+*               -t       :  transpose filter              [FALSE]       *
 *       infile:                                                         *
 *               cepstral coefficients                                   *
 *                       , c~(0), c~(1), ..., c~(M),                     *
@@ -73,7 +74,7 @@
 *                                                                       *
 ************************************************************************/
 
-static char *rcs_id = "$Id: lmadf.c,v 1.24 2009/12/24 18:22:08 uratec Exp $";
+static char *rcs_id = "$Id: lmadf.c,v 1.25 2010/03/23 06:31:56 tatsuyaito Exp $";
 
 
 /*  Standard C Libraries  */
@@ -104,32 +105,29 @@ static char *rcs_id = "$Id: lmadf.c,v 1.24 2009/12/24 18:22:08 uratec Exp $";
 #define PADEORD 4
 #define NGAIN FA
 #define INVERSE   FA
+#define TRANSPOSE FA
 
-char *BOOL[] = { "FALSE", "TRUE" };
+char *BOOL[] = {"FALSE", "TRUE"};
 
 /*  Command Name  */
 char *cmnd;
 
 
-void usage(int status)
+void usage (int status)
 {
    fprintf(stderr, "\n");
-   fprintf(stderr, " %s - LMA digital filter for speech synthesis\n", cmnd);
+   fprintf(stderr, " %s - LMA digital filter for speech synthesis\n",cmnd);
    fprintf(stderr, "\n");
    fprintf(stderr, "  usage:\n");
    fprintf(stderr, "       %s [ options ] cfile [ infile ] > stdout\n", cmnd);
    fprintf(stderr, "  options:\n");
    fprintf(stderr, "       -m m  : order of cepstrum           [%d]\n", ORDER);
-   fprintf(stderr, "       -p p  : frame period                [%d]\n",
-           FPERIOD);
-   fprintf(stderr, "       -i i  : interpolation period        [%d]\n",
-           IPERIOD);
-   fprintf(stderr, "       -P P  : order of Pade approximation [%d]\n",
-           PADEORD);
-   fprintf(stderr, "       -v    : inverse filter              [%s]\n",
-           BOOL[INVERSE]);
-   fprintf(stderr, "       -k    : filtering without gain      [%s]\n",
-           BOOL[NGAIN]);
+   fprintf(stderr, "       -p p  : frame period                [%d]\n", FPERIOD);
+   fprintf(stderr, "       -i i  : interpolation period        [%d]\n", IPERIOD);
+   fprintf(stderr, "       -P P  : order of Pade approximation [%d]\n", PADEORD);
+   fprintf(stderr, "       -v    : inverse filter              [%s]\n", BOOL[INVERSE]);
+   fprintf(stderr, "       -t    : transpose filter            [%s]\n", BOOL[TRANSPOSE]);
+   fprintf(stderr, "       -k    : filtering without gain      [%s]\n", BOOL[NGAIN]);
    fprintf(stderr, "       -h    : print this message\n");
    fprintf(stderr, "  infile:\n");
    fprintf(stderr, "       filter input (%s)                [stdin]\n", FORMAT);
@@ -141,7 +139,7 @@ void usage(int status)
    fprintf(stderr, "       P = 4 or 5\n");
 #ifdef PACKAGE_VERSION
    fprintf(stderr, "\n");
-   fprintf(stderr, " SPTK: version %s\n", PACKAGE_VERSION);
+   fprintf(stderr, " SPTK: version %s\n",PACKAGE_VERSION);
    fprintf(stderr, " CVS Info: %s", rcs_id);
 #endif
    fprintf(stderr, "\n");
@@ -149,116 +147,114 @@ void usage(int status)
 }
 
 
-int main(int argc, char **argv)
+int main (int argc, char **argv)
 {
-   int m = ORDER, fprd = FPERIOD, iprd = IPERIOD, i, j, pd = PADEORD;
-   FILE *fp = stdin, *fpc = NULL;
-   double *c, *inc, *cc, *d, x;
-   Boolean ngain = NGAIN, inverse = INVERSE;
-
-   if ((cmnd = strrchr(argv[0], '/')) == NULL)
-      cmnd = argv[0];
-   else
-      cmnd++;
-   while (--argc)
-      if (**++argv == '-') {
-         switch (*(*argv + 1)) {
-         case 'm':
-            m = atoi(*++argv);
-            --argc;
-            break;
-         case 'p':
-            fprd = atoi(*++argv);
-            --argc;
-            break;
-         case 'i':
-            iprd = atoi(*++argv);
-            --argc;
-            break;
-         case 'P':
-            pd = atoi(*++argv);
-            --argc;
-            break;
-         case 'k':
-            ngain = 1 - ngain;
-            break;
-         case 'v':
-            inverse = 1 - inverse;
-            break;
-         case 'h':
-            usage(0);
-         default:
-            fprintf(stderr, "%s : Invalid option '%c'!\n", cmnd, *(*argv + 1));
-            usage(1);
-         }
-      } else if (fpc == NULL)
-         fpc = getfp(*argv, "rb");
-      else
-         fp = getfp(*argv, "rb");
-
-   if ((pd < 4) || (pd > 5)) {
-      fprintf(stderr, "%s : Order of Pade approximation should be 4 or 5!\n",
-              cmnd);
-      return (1);
-   }
-
-   if (fpc == NULL) {
-      fprintf(stderr, "%s : Cannot open cepstrum file!\n", cmnd);
-      return (1);
-   }
-
-   c = dgetmem(m + m + m + 3 + (m + 1) * pd * 2);
-   cc = c + m + 1;
-   inc = cc + m + 1;
-   d = inc + m + 1;
-
-   if (freadf(c, sizeof(*c), m + 1, fpc) != m + 1)
-      return (1);
-   if (inverse) {
-      if (!ngain) {
-         for (i = 0; i <= m; i++)
-            c[i] *= -1;
-      } else {
-         c[0] = 0;
-         for (i = 1; i <= m; i++)
-            c[i] *= -1;
+  int m=ORDER, fprd=FPERIOD, iprd=IPERIOD, i, j, pd=PADEORD;
+  FILE *fp=stdin, *fpc=NULL;
+  double *c, *inc, *cc, *d, x;
+  Boolean ngain=NGAIN, inverse=INVERSE, transpose=TRANSPOSE;
+  
+  if ((cmnd=strrchr(argv[0], '/'))==NULL)
+    cmnd = argv[0];
+  else
+    cmnd++;
+  while (--argc)
+    if (**++argv=='-') {
+      switch (*(*argv+1)) {
+      case 'm':
+	m = atoi(*++argv);
+	--argc;
+	break;
+      case 'p':
+	fprd = atoi(*++argv);
+	--argc;
+	break;
+      case 'i':
+	iprd = atoi(*++argv);
+	--argc;
+	break;
+      case 't':
+	transpose = 1 - transpose;
+	break;
+      case 'P':
+	pd = atoi(*++argv);
+	--argc;
+	break;
+      case 'k':
+	ngain = 1 - ngain;
+	break;
+      case 'v':
+	inverse = 1 - inverse;
+	break;
+      case 'h':
+	usage (0);
+      default:
+	fprintf(stderr, "%s : Invalid option '%c'!\n", cmnd, *(*argv+1));
+	usage (1);
       }
-   }
-
-   for (;;) {
-      if (freadf(cc, sizeof(*cc), m + 1, fpc) != m + 1)
-         return (0);
+    }
+    else if (fpc==NULL)
+      fpc = getfp(*argv, "rb");
+    else
+      fp = getfp(*argv, "rb");
+  
+  if ((pd<4)||(pd>5)) {
+    fprintf(stderr,"%s : Order of Pade approximation should be 4 or 5!\n",cmnd);
+    return(1);
+  }
+  
+  if (fpc==NULL) {
+    fprintf(stderr,"%s : Cannot open cepstrum file!\n",cmnd);
+    return(1);
+  }
+  
+  c = dgetmem(m+m+m+3+(m+1)*pd*2);
+  cc  = c  + m + 1;
+  inc = cc + m + 1;
+  d   = inc+ m + 1;
+  
+  if (freadf(c, sizeof(*c), m+1, fpc)!=m+1) return(1);
+  if (inverse) {
+    if (!ngain) {
+      for (i=0; i<=m; i++) c[i] *= -1;
+    } else { 
+      c[0] = 0;
+      for (i=1; i<=m; i++) c[i] *= -1;
+    }
+  }
+  
+    for (;;) {
+      if (freadf(cc, sizeof(*cc), m+1, fpc)!=m+1) return(0);
       if (inverse) {
-         if (!ngain) {
-            for (i = 0; i <= m; i++)
-               cc[i] *= -1;
-         } else {
-            cc[0] = 0;
-            for (i = 1; i <= m; i++)
-               cc[i] *= -1;
-         }
+	if (!ngain) {
+	  for (i=0; i<=m; i++) cc[i] *= -1;
+	} else {
+	  cc[0] = 0;
+	  for (i=1; i<=m; i++) cc[i] *= -1;
+	}
       }
-      for (i = 0; i <= m; i++)
-         inc[i] = (cc[i] - c[i]) * iprd / fprd;
-
-      for (j = fprd, i = (iprd + 1) / 2; j--;) {
-         if (freadf(&x, sizeof(x), 1, fp) != 1)
-            return (0);
-
-         if (!ngain)
-            x *= exp(c[0]);
-         x = lmadf(x, c, m, pd, d);
-         fwritef(&x, sizeof(x), 1, stdout);
-
-         if (!--i) {
-            for (i = 0; i <= m; i++)
-               c[i] += inc[i];
+      for (i=0; i<=m; i++)
+	inc[i] = (cc[i] - c[i])*iprd / fprd;
+      
+      for (j=fprd, i=(iprd+1)/2; j--;) {
+	  if (freadf(&x, sizeof(x), 1, fp)!=1) return(0);
+	  
+	  if (!ngain) x *= exp(c[0]);
+	  if (transpose)
+	    x = lmadft(x, c, m, pd, d);
+	  else
+	    x = lmadf(x, c, m, pd, d);
+	  fwritef(&x, sizeof(x), 1, stdout);
+	  
+	  if (!--i) {
+            for (i=0; i<=m; i++) c[i] += inc[i];
             i = iprd;
-         }
+	  }
       }
-
-      movem(cc, c, sizeof(*cc), m + 1);
-   }
-
-   return (0);
+      
+      movem(cc, c, sizeof(*cc), m+1);
+    }
+    
+    return(0);
 }
+
