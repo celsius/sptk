77558b4b7527c635f4c62751131149db1f95f226
Akira Tamamori <mataki@users.sourceforge.net>
2011-11-22 13:37:03 +0000

    delete irregular new line in the SPTK license.

--- src/bin/pitch/snack/jkGetF0.c
+++ src/bin/pitch/snack/jkGetF0.c
@@ -43,7 +43,6 @@
 /*   contributors may be used to endorse or promote products derived */
 /*   from this software without specific prior written permission.   */
 /*                                                                   */
-
 /* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND            */
 /* CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,       */
 /* INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF          */
