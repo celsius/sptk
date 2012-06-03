97e502c49121614bb09d6b552cf3956a8a957905
Akira Tamamori <mataki@users.sourceforge.net>
2012-06-03 03:55:03 +0000

    move bin/window/_window.c to lib/window.c

--- src/bin/mfcc/_mfcc.c
+++ src/bin/mfcc/_mfcc.c
@@ -483,7 +483,7 @@ void mfcc(double *in, double *mc, const double sampleFreq, const double alpha,
    pre_emph(x, px, alpha, wlng);
    /* apply hamming window */
    if (usehamming)
-      window(HAMMING, px, sizeof(*px), wlng);
+      window(HAMMING, px, wlng, 0);
    for (k = 0; k < wlng; k++)
       wx[k] = px[k];
    spec(wx, sp, flng);
