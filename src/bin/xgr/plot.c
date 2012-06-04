bb8e5b885f7228f688518180b8b28715ad1a3ff5
okdtmhr <okdtmhr@users.sourceforge.net>
2012-06-04 23:46:26 +0000

    remove unused function and so on

--- src/bin/xgr/plot.c
+++ src/bin/xgr/plot.c
@@ -95,10 +95,114 @@ static char *symbol_lower = "abgdezhqiklmnxoprstufcyw";
 static void line(XPoint * points, int n);
 static void fillpoly(XPoint * points, int type, int n);
 static void reset_fill(void);
-static void dplot(int density, short x, short y, short w, short h);
+/*static void dplot(int density, short x, short y, short w, short h);*/
 static void fillbox(int type, short x, short y, short w, short h);
 static int text(char *s, int n, int fn);
 
+/*
+ *	Color name data
+ */
+static char *c_name[] = { "black", "black", "blue",
+   "red", "medium sea green",
+   "violet", "orange", "turquoise"
+};
+
+/*
+ *	Line width data
+ */
+static int l_width[] = { 0, 1, 1, 2, 3, 4, 1, 2, 3, 4 };
+
+/*
+ *	Line style data
+ */
+static struct line_style_info {
+   char list[8];
+   int no;
+} l_style[] = { { {
+1, 1}, 2},                      /* Dummy for Solid */
+{ {
+1, 5}, 2},                      /* dotted2 */
+{ {
+1, 3}, 2},                      /* dotted */
+{ {
+4, 2}, 2},                      /* dashed */
+{ {
+7, 3}, 2},                      /* long_dashed  */
+{ {
+1, 1}, 2},                      /* Dummy for Solid */
+{ {
+8, 2, 2, 2}, 4},                /* dot_dashed */
+{ {
+10, 2, 2, 2}, 4},               /* dot_dashed2  */
+{ {
+8, 2, 1, 2, 1, 2}, 6},          /* double_dot_dashed */
+{ {
+6, 2, 2, 2, 2, 2}, 6}           /* double_dot_dashed2 */
+};
+
+
+/*
+ * Mark bitmap data
+ */
+#define mark_width  9
+#define mark_height 9
+
+#include "marks/mark0.dat"
+#include "marks/mark1.dat"
+#include "marks/mark2.dat"
+#include "marks/mark3.dat"
+#include "marks/mark4.dat"
+#include "marks/mark5.dat"
+#include "marks/mark6.dat"
+#include "marks/mark7.dat"
+#include "marks/mark8.dat"
+#include "marks/mark9.dat"
+#include "marks/mark10.dat"
+#include "marks/mark11.dat"
+#include "marks/mark12.dat"
+#include "marks/mark13.dat"
+#include "marks/mark14.dat"
+#include "marks/mark15.dat"
+
+static char *mark_bits[] = {
+   mark0_bits, mark1_bits, mark2_bits, mark3_bits,
+   mark4_bits, mark5_bits, mark6_bits, mark7_bits,
+   mark8_bits, mark9_bits, mark10_bits, mark11_bits,
+   mark12_bits, mark13_bits, mark14_bits, mark15_bits
+};
+
+
+/*
+ * Till Pattern
+ */
+#define till_width 4
+#define till_height 4
+
+#include "tills/till0.dat"
+#include "tills/till1.dat"
+#include "tills/till2.dat"
+#include "tills/till3.dat"
+#include "tills/till4.dat"
+#include "tills/till5.dat"
+#include "tills/till6.dat"
+#include "tills/till7.dat"
+#include "tills/till8.dat"
+#include "tills/till9.dat"
+#include "tills/till10.dat"
+#include "tills/till11.dat"
+#include "tills/till12.dat"
+#include "tills/till13.dat"
+#include "tills/till14.dat"
+#include "tills/till15.dat"
+#include "tills/till16.dat"
+
+static char *till_bits[] = {
+   till0_bits, till1_bits, till2_bits, till3_bits,
+   till4_bits, till5_bits, till6_bits, till7_bits,
+   till8_bits, till9_bits, till10_bits, till11_bits,
+   till12_bits, till13_bits, till14_bits, till15_bits,
+   till16_bits
+};
 
 static short normy(int y)
 {
@@ -173,7 +277,7 @@ static void polyline(XPoint * points, int frame, int fill, int n)
       line(points, n + 1);
 }
 
-static int polyg(int type)
+/*static int polyg(int type)
 {
    int x, y, w, h;
 
@@ -181,12 +285,12 @@ static int polyg(int type)
    dplot(type, normx(x), normy(y + h), norm(w), norm(h));
 
    return (0);
-}
+   }*/
 
 #define LEVEL 256
 #define POINTS 1024
 
-static void dplot(int density, short x, short y, short w, short h)
+ /*static void dplot(int density, short x, short y, short w, short h)
 {
    int n;
    int n_max, n_plot, flg[POINTS];
@@ -211,7 +315,7 @@ static void dplot(int density, short x, short y, short w, short h)
       }
    }
    XDrawPoints(display, main_window, gc, pos, n_plot, CoordModeOrigin);
-}
+   }*/
 
 static void hatching(int type)
 {
@@ -257,10 +361,10 @@ static void reset_fill(void)
    fillbox(15, 0, 0, 0, 0);
 }
 
-static void box(short x, short y, short w, short h)
+/*static void box(short x, short y, short w, short h)
 {
    XDrawRectangle(display, main_window, gc, x, y, w, h);
-}
+   }*/
 
 static void fillbox(int type, short x, short y, short w, short h)
 {
@@ -377,7 +481,7 @@ static int text(char *s, int n, int fn)
          cx = points[0].x - xadj;
          cy = points[0].y - yadj * --n;
          XDrawString(display, main_window, gc, cx, cy, s, 1);
-         *s++;
+         s++;
       }
    }
 
