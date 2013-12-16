9bbd76884a18da543e2cb1893683ce114fc64471
Akira Tamamori <mataki@users.sourceforge.net>
2013-12-16 09:01:53 +0000

    update all documents for version 3.7.

--- src/bin/decimate/decimate.c
+++ src/bin/decimate/decimate.c
@@ -8,7 +8,7 @@
 /*                           Interdisciplinary Graduate School of    */
 /*                           Science and Engineering                 */
 /*                                                                   */
-/*                1996-2012  Nagoya Institute of Technology          */
+/*                1996-2013  Nagoya Institute of Technology          */
 /*                           Department of Computer Science          */
 /*                                                                   */
 /* All rights reserved.                                              */
@@ -63,8 +63,7 @@
 *                                                                       *
 ************************************************************************/
 
-static char *rcs_id =
-    "$Id$";
+static char *rcs_id = "$Id$";
 
 
 /*  Standard C Libraries  */
