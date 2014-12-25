a760d9c012e3c4785b80085dbb21f59eae1efa9e
Keiichiro Oura <uratec@users.sourceforge.net>
2014-12-25 02:41:38 +0000

    fix code format.

--- src/bin/lspcheck/lspcheck.c
+++ src/bin/lspcheck/lspcheck.c
@@ -79,8 +79,7 @@
 *                                                                       *
 ************************************************************************/
 
-static char *rcs_id =
-    "$Id$";
+static char *rcs_id = "$Id$";
 
 
 /*  Standard C Libraries  */
